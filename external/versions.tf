d611e2e63833fcf9c894932366fabd5c559d8aeb
Jonathan Goren <jonagn@gmail.com>
2024-03-05 12:53:57 +0200

    initial configure

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "yogocasa"
 
     workspaces {
       name = "homelab-external"
--- platform/gitea/files/config/go.mod
+++ platform/gitea/files/config/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/gitea/config
+module git.yogo.casa/khuedoan/homelab/gitea/config
 
 go 1.19
 
